e91386d39051ca57b4809c2475e76a7c8193569b
Thu <thu.nguyen22@student.passerellesnumeriques.org>
2022-07-13 11:51:59 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 import FoodSearch from './pages/homePage';
 import Quantity from './pages/quantity';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
+
     // <ListCars/>
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={< FoodSearch/>}>
+      
         <Route index element={<FoodSearch />} />
         <Route path="/quantity" element={<Quantity />} />
-     </Route>
+
     </Routes>
   </BrowserRouter>
   );
